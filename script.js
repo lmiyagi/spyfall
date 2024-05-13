97fe1b9793633dd8ab473d496cab0064aa2c4d2c
L. Miyagi <lmiyagi@pop-os.localdomain>
2024-05-14 04:34:15 +0900

    adding styles and js scripts

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("switch");
+const sectionNumOfPlayers = document.getElementById("sectionNumOfPlayers");
+
+button.addEventListener("click", function() {
+    sectionNumOfPlayers.style.display = "none";
+});
