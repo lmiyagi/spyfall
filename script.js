914ef086b4b09abae49b3ea6afdcce5ac546bfba
L. Miyagi <lmiyagi@pop-os.localdomain>
2024-05-31 13:45:45 +0900

    adding example modal, better handling buttons

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
-const button = document.getElementById("switch");
-const sectionNumOfPlayers = document.getElementById("sectionNumOfPlayers");
+const Pages = {
+    Players: 'players',
+    Game: 'game'
+}
 
-button.addEventListener("click", function() {
-    sectionNumOfPlayers.style.display = "none";
+const sectionNumOfPlayers = document.getElementById("sectionNumOfPlayers");
+const btnAddPlayer = document.getElementById("btnAddPlayer");
+const btnRemovePlayer = document.getElementById("btnRemovePlayer");
+const textNumOfPlayers = document.getElementById("textNumOfPlayers");
+const btnOkPlayers = document.getElementById("btnOkPlayers");
+const mainModal = $('#mainModal');
+const mainModalTitle = $('#mainModalTitle');
+const mainModalText = $('#mainModalText');
+
+
+// Number of players ----------------------------------------
+var numOfPlayers = 0;
+
+btnAddPlayer.addEventListener("click", function() {
+    numOfPlayers++;
+    displayNumOfPlayers(numOfPlayers);
 });
+
+btnRemovePlayer.addEventListener("click", function() {
+    numOfPlayers--;
+    displayNumOfPlayers(numOfPlayers);
+});
+
+btnOkPlayers.addEventListener("click", function() {
+    showModal("Title test", "Test Test Test Test Test Test Test Test Test");
+});
+
+function displayNumOfPlayers(numOfPlayers) {
+    textNumOfPlayers.innerHTML = numOfPlayers;
+}
+// ----------------------------------------------------------
+
+function getLocationsFromFile() {
+    const req = new XMLHttpRequest();
+    req.open('GET', window.location.protocol + '//' + window.location.host + '/locations.txt', false);
+    req.send();
+ 
+    return req.responseText.split(/[\n\r]+/);
+ }
+
+ function showModal(title, text) {
+
+    mainModalTitle.innerHTML = title
+    mainModalText.innerHTML = text
+    mainModal.modal('show')
+
+ }
